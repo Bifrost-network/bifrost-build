bfba8bae96ee361d05188a5f3873fabbf067a5a0
Jens Låås <jens@jelaas.eu>
2017-10-04 14:50:27 +0200

    fix source

--- all/expat-2.0.1-1/Fetch-source.sh
+++ all/expat-2.0.1-1/Fetch-source.sh
@@ -2,5 +2,8 @@
 
 SRC=expat-2.0.1.tar.gz
 DST=/var/spool/src/$SRC
+SHA=b6e5a9979d9652f00fed657357ad41306927f51e526a9b6105633d519f2fc1df
 
-[ -s "$DST" ] || wget -O $DST http://heanet.dl.sourceforge.net/project/expat/expat/2.0.1/$SRC
+pkg_install tarmd-1.2-1 || exit 2
+pkg_install curl-7.51.0-1 || exit 2
+[ -s "$DST" ] || tarmd $SHA $DST curl -L -k https://downloads.sourceforge.net/project/expat/expat/2.0.1/$SRC
