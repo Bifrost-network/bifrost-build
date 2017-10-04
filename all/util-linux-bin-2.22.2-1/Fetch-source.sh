5555257e30be07fcdecb1339697209c9ae2d6409
Jens Låås <jens@jelaas.eu>
2017-10-04 13:43:46 +0200

    fix source

--- all/util-linux-bin-2.22.2-1/Fetch-source.sh
+++ all/util-linux-bin-2.22.2-1/Fetch-source.sh
@@ -2,5 +2,9 @@
 
 SRC=util-linux-2.22.2.tar.bz2
 DST=/var/spool/src/$SRC
+SHA=f4453349e51e2d55ae32eddf6ce9ba5ecbd391ffa591f95e62f511ccb9458b9a
 
-[ -s "$DST" ] || wget -O $DST ftp://ftp.kernel.org/pub/linux/utils/util-linux/v2.22/$SRC
+pkg_install tarmd-1.2-1 || exit 2
+pkg_install curl-7.51.0-1 || exit 2
+[ -s "$DST" ] || tarmd $SHA $DST curl -L -k https://www.kernel.org/pub/linux/utils/util-linux/v2.22/$SRC
+
