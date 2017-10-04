fcafec9d1d1f8372a947f967a717e4cb430cd10d
Jens Låås <jens@jelaas.eu>
2017-10-04 15:34:18 +0200

    fix source

--- all/dialog-1.1-20100428-1/Fetch-source.sh
+++ all/dialog-1.1-20100428-1/Fetch-source.sh
@@ -2,5 +2,10 @@
 
 SRC=dialog-1.1-20100428.tgz
 DST=/var/spool/src/$SRC
+SHA=bde67490bc540e9e6396728dcf1499261e6b0bc9668360744020ecdb83f811cf
 
-[ -s "$DST" ] || wget -O $DST ftp://invisible-island.net/dialog/$SRC
+pkg_install tarmd-1.2-1 || exit 2
+pkg_install curl-7.51.0-1 || exit 2
+[ -s "$DST" ] || tarmd $SHA $DST curl -L -k https://invisible-mirror.net/archives/dialog/$SRC
+
+
