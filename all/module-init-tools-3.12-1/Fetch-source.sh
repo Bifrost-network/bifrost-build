b10a5725aa1e8438dda329ee0f5cab6b2cd4d068
Jens Låås <jens@jelaas.eu>
2017-10-05 15:07:33 +0200

    fix source

--- all/module-init-tools-3.12-1/Fetch-source.sh
+++ all/module-init-tools-3.12-1/Fetch-source.sh
@@ -2,5 +2,9 @@
 
 SRC=module-init-tools-3.12.tar.bz2
 DST=/var/spool/src/$SRC
+SHA=4409a1fd92538c5bf255b7117abb84e617057f5d3f6020d2b01d156de833dc78
 
-[ -s "$DST" ] || wget -O $DST ftp://ftp.kernel.org/pub/linux/utils/kernel/module-init-tools/$SRC
+pkg_install tarmd-1.2-1 || exit 2
+pkg_install curl-7.51.0-1 || exit 2
+[ -s "$DST" ] || tarmd $SHA $DST curl -L -k https://www.kernel.org/pub/linux/utils/kernel/module-init-tools/$SRC
+
