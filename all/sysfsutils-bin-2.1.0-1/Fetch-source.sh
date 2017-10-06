6faaf1645bbf47ccbcd172463058ce2e32abb3a7
Jens Låås <jens@jelaas.eu>
2017-10-06 09:42:19 +0200

    fix source

--- all/sysfsutils-bin-2.1.0-1/Fetch-source.sh
+++ all/sysfsutils-bin-2.1.0-1/Fetch-source.sh
@@ -2,5 +2,8 @@
 
 SRC=sysfsutils-2.1.0.tar.gz
 DST=/var/spool/src/$SRC
+SHA=cc8732f44dcbc6293c2b04e3c3340de2451043ada31586e0e945a0732237b8f9
 
-[ -s "$DST" ] || wget -O $DST http://downloads.sourceforge.net/project/linux-diag/sysfsutils/2.1.0/$SRC
+pkg_install tarmd-1.2-1 || exit 2
+pkg_install curl-7.51.0-1 || exit 2
+[ -s "$DST" ] || tarmd $SHA $DST curl -L -k http://downloads.sourceforge.net/project/linux-diag/sysfsutils/2.1.0/$SRC
