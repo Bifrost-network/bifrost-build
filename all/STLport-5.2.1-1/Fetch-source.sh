66d74a739ac186b5fa386bb2f6e0c9b42b589c30
Jens Låås <jens@jelaas.eu>
2017-10-06 13:08:24 +0200

    fix source

--- all/STLport-5.2.1-1/Fetch-source.sh
+++ all/STLport-5.2.1-1/Fetch-source.sh
@@ -2,5 +2,8 @@
 
 SRC=STLport-5.2.1.tar.bz2
 DST=/var/spool/src/$SRC
+SHA=f067e82736bc0f24646e1cfc59e5f80d67b1f5f66e3d7fb704b62fa5f6a83e41
 
-[ -s "$DST" ] || wget -O $DST http://downloads.sourceforge.net/project/stlport/STLport/STLport-5.2.1/$SRC
+pkg_install tarmd-1.2-1 || exit 2
+pkg_install curl-7.51.0-1 || exit 2
+[ -s "$DST" ] || tarmd $SHA $DST curl -L -k http://downloads.sourceforge.net/project/stlport/STLport/STLport-5.2.1/$SRC
