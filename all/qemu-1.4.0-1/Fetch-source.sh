3f3a3eee3b93dc502c95c2594528fbf8367f1b61
Jens Låås <jens@jelaas.eu>
2017-10-06 13:35:43 +0200

    fix source

--- all/qemu-1.4.0-1/Fetch-source.sh
+++ all/qemu-1.4.0-1/Fetch-source.sh
@@ -2,5 +2,8 @@
 
 SRC=qemu-1.4.0.tar.bz2
 DST=/var/spool/src/$SRC
+SHA=17dbabb2a807e6fdec8256d1c54eb2773902c73442dd14d6ae94615f5566e741
 
-[ -s "$DST" ] || wget -O $DST http://wiki.qemu-project.org/download/$SRC
+pkg_install tarmd-1.2-1 || exit 2
+pkg_install curl-7.51.0-1 || exit 2
+[ -s "$DST" ] || tarmd $SHA $DST curl -L -k https://wiki.qemu-project.org/download/$SRC
