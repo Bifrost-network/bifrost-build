1b7c20c1acc792b8b93444f796eb9c7fe7f5c78a
Jens Låås <jens@jelaas.eu>
2017-10-06 12:32:52 +0200

    fix source

--- all/ipgrab-0.9.10-1/Fetch-source.sh
+++ all/ipgrab-0.9.10-1/Fetch-source.sh
@@ -2,5 +2,8 @@
 
 SRC=ipgrab-0.9.10.tar.gz
 DST=/var/spool/src/$SRC
+SHA=7b0c73321d3fa129cbfa1f175114ed766e5752039e217e2b4130bddb448bbea6
 
-[ -s "$DST" ] || wget -O $DST http://downloads.sourceforge.net/project/ipgrab/ipgrab/0.9.10/$SRC
+pkg_install tarmd-1.2-1 || exit 2
+pkg_install curl-7.51.0-1 || exit 2
+[ -s "$DST" ] || tarmd $SHA $DST curl -L -k https://downloads.sourceforge.net/project/ipgrab/ipgrab/0.9.10/$SRC
