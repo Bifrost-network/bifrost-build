989205df5efced7165d934c5275249aa26f31bbb
Jens Låås <jens@jelaas.eu>
2017-10-06 10:36:55 +0200

    fix source

--- all/libnet-1.1.4-1/Fetch-source.sh
+++ all/libnet-1.1.4-1/Fetch-source.sh
@@ -2,5 +2,8 @@
 
 SRC=libnet-1.1.4.tar.gz
 DST=/var/spool/src/$SRC
+SHA=ac5051a9f3b2e30f5b989df8cf7fe300c9165a0f35e4a679c9c3d1ffa92f3c92
 
-[ -s "$DST" ] || wget -O $DST http://downloads.sourceforge.net/project/libnet-dev/$SRC
+pkg_install tarmd-1.2-1 || exit 2
+pkg_install curl-7.51.0-1 || exit 2
+[ -s "$DST" ] || tarmd $SHA $DST curl -L -k https://downloads.sourceforge.net/project/libnet-dev/$SRC
