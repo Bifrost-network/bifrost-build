29fb39ea7397fd1b5de62650985b04fbccd73eac
Jens Låås <jens@jelaas.eu>
2017-10-06 10:29:51 +0200

    fix source

--- all/libusb-1.0.8-1/Fetch-source.sh
+++ all/libusb-1.0.8-1/Fetch-source.sh
@@ -2,5 +2,8 @@
 
 SRC=libusb-1.0.8.tar.bz2
 DST=/var/spool/src/$SRC
+SHA=10016452e272a35a6576e605d1cdef2764a3147c56cd5a37863c9d948c18188e
 
-[ -s "$DST" ] || wget -O $DST http://downloads.sourceforge.net/project/libusb/libusb-1.0/libusb-1.0.8/$SRC
+pkg_install tarmd-1.2-1 || exit 2
+pkg_install curl-7.51.0-1 || exit 2
+[ -s "$DST" ] || tarmd $SHA $DST curl -L -k http://downloads.sourceforge.net/project/libusb/libusb-1.0/libusb-1.0.8/$SRC
