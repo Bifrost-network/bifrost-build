47ac3e8c793c0b26adcc3dcd9a3e5a7d1e051e21
Jens Låås <jens@jelaas.eu>
2017-10-06 13:21:48 +0200

    fix source

--- all/util-linux-lib-2.20.1-1/Fetch-source.sh
+++ all/util-linux-lib-2.20.1-1/Fetch-source.sh
@@ -2,5 +2,8 @@
 
 SRC=util-linux-2.20.1.tar.bz2
 DST=/var/spool/src/$SRC
+SHA=082306ec5d0ca0279778ee4f79b3cef876f2bc6cdf105d7bc50cc6f1de667f3f
 
-[ -s "$DST" ] || wget -O $DST ftp://ftp.kernel.org/pub/linux/utils/util-linux/v2.20/$SRC
+pkg_install tarmd-1.2-1 || exit 2
+pkg_install curl-7.51.0-1 || exit 2
+[ -s "$DST" ] || tarmd $SHA $DST curl -L -k https://www.kernel.org/pub/linux/utils/util-linux/v2.20/$SRC
