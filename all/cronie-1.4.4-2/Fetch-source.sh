6726e51d7c21a4357f0d277a326e7f6ad0966d79
Jens Låås <jens@jelaas.eu>
2017-10-09 16:19:26 +0200

    fix source

--- all/cronie-1.4.4-2/Fetch-source.sh
+++ all/cronie-1.4.4-2/Fetch-source.sh
@@ -2,7 +2,8 @@
 
 SRC=cronie-1.4.4.tar.gz
 DST=/var/spool/src/$SRC
+SHA=8f0ff0dc98582981f93b077527daf87d45dfd0a9f2e9294480d5fbb510317427
 
-pkg_install wget-1.12-1 || exit 1
-
-[ -s "$DST" ] || wget --no-check-certificate -O $DST http://fedorahosted.org/releases/c/r/cronie/$SRC
+pkg_install tarmd-1.2-1 || exit 2
+pkg_install curl-7.51.0-1 || exit 2
+[ -s "$DST" ] || tarmd $SHA $DST curl -L -k http://people.redhat.com/tmraz/cronie/$SRC
