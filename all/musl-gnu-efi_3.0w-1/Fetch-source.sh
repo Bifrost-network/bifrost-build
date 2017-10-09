c4ed90eeacef90252f726a70d9a9a7303c5a3dd8
Jens Låås <jens@jelaas.eu>
2017-10-09 09:50:51 +0200

    fix source

--- all/musl-gnu-efi_3.0w-1/Fetch-source.sh
+++ all/musl-gnu-efi_3.0w-1/Fetch-source.sh
@@ -2,5 +2,8 @@
 
 SRC=gnu-efi_3.0w.orig.tar.gz
 DST=/var/spool/src/$SRC
+SHA=31bbc46eed17e08c48cfbf72947350c92e96c23cc6272f4bf3d7cd1a88a4e94f
 
-[ -s "$DST" ] || wget -O $DST http://downloads.sourceforge.net/project/gnu-efi/$SRC
+pkg_install tarmd-1.2-1 || exit 2
+pkg_install curl-7.51.0-1 || exit 2
+[ -s "$DST" ] || tarmd $SHA $DST curl -L -k https://downloads.sourceforge.net/project/gnu-efi/$SRC
