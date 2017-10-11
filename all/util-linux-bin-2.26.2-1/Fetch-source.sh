3a4e6c7cb1443323bcabed28a5c4fa9871b90946
Jens Låås <jens@jelaas.eu>
2017-10-11 09:19:15 +0200

    fix source

--- all/util-linux-bin-2.26.2-1/Fetch-source.sh
+++ all/util-linux-bin-2.26.2-1/Fetch-source.sh
@@ -2,5 +2,8 @@
 
 SRC=util-linux-2.26.2.tar.gz
 DST=/var/spool/src/$SRC
+SHA=c8f7f71bd9a6cd1ade6df15d352f2ac9c576dc95b9d2945f079bb33db3eeb3fb
 
-[ -s "$DST" ] || wget -O $DST ftp://ftp.kernel.org/pub/linux/utils/util-linux/v2.26/$SRC
+pkg_install tarmd-1.2-1 || exit 2
+pkg_install curl-7.51.0-1 || exit 2
+[ -s "$DST" ] || tarmd $SHA $DST curl -L -k https://www.kernel.org/pub/linux/utils/util-linux/v2.26/$SRC
