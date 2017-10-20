fa8b1f35822d0a006af2fcbfaf70f9da9ffa2cde
Jens Låås <jens@jelaas.eu>
2017-10-20 12:51:23 +0200

    fix source

--- all/musl-tcp_wrappers-7.6-1/Fetch-source.sh
+++ all/musl-tcp_wrappers-7.6-1/Fetch-source.sh
@@ -2,5 +2,8 @@
 
 SRC=tcp_wrappers_7.6.tar.gz
 DST=/var/spool/src/$SRC
+SHA=82bd9584b20f0f3d2a2af9100acd63ddfd9f123aa2546c7be5007550bfbcaa10
 
-[ -s "$DST" ] || wget -O $DST ftp://ftp.porcupine.org/pub/security/$SRC
+pkg_install tarmd-1.2-1 || exit 2
+pkg_install curl-7.51.0-1 || exit 2
+[ -s "$DST" ] || tarmd $SHA $DST curl -L -k ftp://ftp.porcupine.org/pub/security/$SRC
