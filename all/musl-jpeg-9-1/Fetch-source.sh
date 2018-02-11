080075fe8069260809c0f790281ab31a28ae2a3d
Jens Låås <jens@jelaas.eu>
2018-02-11 12:33:16 +0100

    fix source

--- all/musl-jpeg-9-1/Fetch-source.sh
+++ all/musl-jpeg-9-1/Fetch-source.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 SRC=jpegsrc.v9.tar.gz
 DST=/var/spool/src/$SRC
+SHA=577548aca8ca5140872cba68dafa7469ca5761f03d2776e273c1ecac675e1fc9
 
-[ -s "$DST" ] || wget -O $DST http://www.ijg.org/files/$SRC
-
+pkg_install curl-7.51.0-1 || exit 2
+pkg_install tarmd-1.2-1 || exit 2
+[ -s "$DST" ] || tarmd $SHA $DST curl -L -k http://www.ijg.org/files/$SRC
